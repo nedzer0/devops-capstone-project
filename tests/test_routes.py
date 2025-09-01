bdbdca33aa562d87aa9cc4ab0556b708bdae2d39
nedzer0 <alexandre.oliveira@campus.fct.unl.pt>
2025-09-01 09:57:35 -0400

    changed test_routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -25,9 +25,6 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
-
-
-
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
